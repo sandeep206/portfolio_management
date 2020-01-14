da493dfaa82d214414646a0dc7b0ee96aa264c41
Sasidharan, Sandeep (ITCDCC) - KLM <Sandeep.Sasidharan@klm.com>
2020-01-14 06:50:59 +0100

    initial commit with project scaffolding.

---
+++ src/__tests__/server.spec.js
@@ -0,0 +1,5 @@
+describe('dummy test', () => {
+  test('should run dummy test', () => {
+    expect(true).toBe(true)
+  })
+})
---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
+import cors from 'cors'
+import { connect } from './utils/db'
+import config from './config'
+
+export const app = express()
+
+app.disable('x-powered-by')
+
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+export const start = async () => {
+  try {
+    await connect()
+    app.listen(config.port, () => {
+      console.log(`Rest API  on http://localhost:${config.port}/api`)
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
